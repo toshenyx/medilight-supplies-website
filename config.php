1d73d83ddb004215781da14756c2fe148d788f9f
Fana Nadiah <mutahinadiah@gmail.com>
2025-11-20 12:02:14 +0300

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 // Database Configuration
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', '123');
 define('DB_NAME', 'medilight_db');
 
 // Create connection function
